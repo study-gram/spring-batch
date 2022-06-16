2f49e39f042040282affb6257fae4f9d8d15ed22
진우 <nice7677@naver.com>
2022-06-16 22:08:04 +0900

    4장 중간 검증

--- batch-practice/src/main/java/kr/springboot/batchpractice/job/HelloWorld.java
+++ batch-practice/src/main/java/kr/springboot/batchpractice/job/HelloWorld.java
@@ -1,14 +1,24 @@
 package kr.springboot.batchpractice.job;
 
+import kr.springboot.batchpractice.valid.ParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -28,16 +38,65 @@ public class HelloWorld {
     public Job job() {
         return this.jobBuilderFactory.get("basicJob")
                 .start(step1())
+                .validator(validator())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    private Step step1() {
+    public Step step1() {
         return this.stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("Hello, World");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                .tasklet(helloWorldTasklet(null, null)).build();
+
+    }
+
+    /**
+     * JobParameters에 접근하기
+     * @return
+     */
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName) {
+
+        return (contribution, chunkContext) -> {
+            log.info("{}", String.format("Hello, %s", name));
+            log.info("{}", String.format("fileName = %s", fileName));
+            return RepeatStatus.FINISHED;
+        };
+
+    }
+
+//    @Bean
+//    public JobParametersValidator validator() {
+//
+//        DefaultJobParametersValidator validator = new DefaultJobParametersValidator();
+//
+//        validator.setRequiredKeys(new String[]{"fileName"});
+//        validator.setOptionalKeys(new String[]{"name"});
+//
+//        return validator;
+//
+//    }
+
+    @Bean
+    public CompositeJobParametersValidator validator() {
+
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+                new String[]{"fileName"},
+                new String[]{"name"}
+        );
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new ParameterValidator(), defaultJobParametersValidator)
+        );
+
+        return validator;
 
     }
 
